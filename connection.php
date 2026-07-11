23385c14d00a47a3c28d063ea76ceedac5b293dc
SrgntSwrd <lesternieva132@gmail.com>
2026-07-11 09:58:50 +0800

    SQLite-Termux-update6
    add the exec('PRAGMA journal_mode = WAL;') to allow differnt dviices towrite on the database

--- connection.php
+++ connection.php
@@ -19,6 +19,9 @@
         $conn = new PDO("sqlite:" . __DIR__ . "/attendance.db");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        
+        $conn->exec('PRAGMA journal_mode = WAL;');
+        
         $conn->exec('PRAGMA foreign_keys = ON;'); // SQLite needs this explicitly turned on
     } catch (PDOException $e) {
         die("Connection failed: " . $e->getMessage());
